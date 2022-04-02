d5f83f86c4c61f15c06c0896435c50bc2ffb0517
HOSTNAME47 <mouadstev1@gmail.com>
2022-04-02 03:18:52 +0000

    add login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,12 +5,12 @@
 @endsection
 
 @push('styles')
-    <link href="{{ asset('css/auth/login.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
 @endpush
 
 @section('content')
-<div id="login-section" class="full-center">
-    <div id="auth-viewer-box" class="viewer-box-style-1">
+<div id="auth-section" class="full-center">
+    <div id="auth-viewer-box">
         @include('partials.auth.login-form')
     </div>
 </div>
