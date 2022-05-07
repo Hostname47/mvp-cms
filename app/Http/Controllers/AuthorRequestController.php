9658cf05a634c98c6e1bd943007e6cabb5e23a28
HOSTNAME47 <mouadstev1@gmail.com>
2022-05-07 14:45:04 +0000

    handle case of submited author request

--- app/Http/Controllers/AuthorRequestController.php
+++ app/Http/Controllers/AuthorRequestController.php
@@ -6,14 +6,28 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\Models\AuthorRequest;
 use App\Models\Category;
+use Purifier;
 
 class AuthorRequestController extends Controller
 {
     public function index(Request $request) {
         $categories = Category::tree()->get()->toTree();
+        $status = 'fresher';
+        $application = null;
+
+        if(auth()->user()) {
+            if($application = AuthorRequest::where('user_id', auth()->user()->id)->first()) {
+                if($application->status)
+                    $status = 'applied-status-approved';
+                else
+                    $status = 'applied-status-not-yet';
+            }
+        }
 
         return view('author-request')
-            ->with(compact('categories'));
+            ->with(compact('categories'))
+            ->with(compact('application'))
+            ->with(compact('status'));
     }
 
     public function request(Request $request) {
@@ -27,11 +41,10 @@ class AuthorRequestController extends Controller
         ]);
 
         $this->authorize('request', [AuthorRequest::class]);
-        
+
+        $data['message'] = Purifier::clean($data['message']);
         $data['categories'] = implode(',', $data['categories']);
         $data['user_id'] = auth()->user()->id;
         AuthorRequest::create($data);
-
-        Session::flash('message', __('You request has been sent successfully. We are going to inform you about approval as soon as possible.'));
     }
 }
--- app/Models/AuthorRequest.php
+++ app/Models/AuthorRequest.php
@@ -4,10 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+use Carbon\Carbon;
 
 class AuthorRequest extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function categories($get='titles') {
+        switch($get) {
+            case 'titles':
+                return Category::findMany(explode(',', $this->categories))->pluck('title');
+                break;
+            case 'all':
+                return Category::findMany(explode(',', $this->categories));
+                break;
+        }
+    }
+
+    public function getDateAttribute() {
+        return (new Carbon($this->created_at))->isoFormat("dddd D MMM YYYY - H:mm A");
+    }
+
+    public function getUpdateDateAttribute() {
+        return (new Carbon($this->updated_at))->isoFormat("dddd D MMM YYYY - H:mm A");
+    }
 }
